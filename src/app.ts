9885fa0597d9bfcaf510ea2db281abdb5b9ebbc4
Md Azad Hossain <sokalazad@gmail.com>
2025-07-08 16:40:44 +0200

    added route module to maintain routes dynamically

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express, { Request, Response } from "express";
 import "dotenv/config";
 import { userRouter } from "./app/modules/user/user.routes";
 import cors from "cors";
+import { router } from "./app/routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({ message: "Server is running for tour management." });
