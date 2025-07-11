d1b0d7d7cf98ca44729e364ff24967b0e157594c
Md Azad Hossain <sokalazad@gmail.com>
2025-07-11 19:23:45 +0200

    added validation middleware for validate user inputs

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validation } from "../../middlewares/responceValidation";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/register", userController.createUser);
+router.post(
+  "/register",
+  validation(createUserZodSchema),
+  userController.createUser
+);
 router.get("/all-users", userController.getAllUsers);
 router.get("/:id", userController.getSingleUser);
 router.delete("/:id", userController.deleteUser);
