46b6074cc7aeba1ebd420606ef17ef8a9b8865e9
Md Azad Hossain <sokalazad@gmail.com>
2025-07-11 23:21:46 +0200

    added password hashing system before save to database

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -2,19 +2,26 @@ import { StatusCodes } from "http-status-codes";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import AppError from "../../errorHelpers/AppError";
+import bcrypt from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
   const isUserExist = await User.findOne({ email });
   if (isUserExist) {
     throw new AppError(StatusCodes.BAD_REQUEST, "User already exist");
   }
+  const hashedPassword = await bcrypt.hash(password as string, 10);
   const authProvider: IAuthProvider = {
     provider: "credentials",
     providerId: email as string,
   };
 
-  const user = await User.create({ email, auths: [authProvider], ...rest });
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+    auths: [authProvider],
+    ...rest,
+  });
 
   return user;
 };
